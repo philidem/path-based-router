761c587c091d58e8a846172d3e7eaa8aa960f4db
cava23 <andrewj.crowell@gmail.com>
2014-01-24 09:27:13 -0500

    Move pattern definition outside of if/else block.

--- amd-router.js
+++ amd-router.js
@@ -125,9 +125,9 @@
                 }
             }
 
+            var pattern = '^' + regexPattern.join('\\/') + '/?$';
             if (placeholders.length > 0) {
                 // combine the regex parts to form the final regex pattern
-                var pattern = '^' + regexPattern.join('\\/') + '/?$';
                 return new Route({
                     route : route,
                     tokens : tokens,
@@ -136,7 +136,6 @@
                 });
             } else {
                 // path contains no placeholders so we will only need to check for exact match
-                var pattern = '^' + regexPattern.join('\\/') + '/?$';
                 return new Route({
                     route : route,
                     regex : new RegExp(pattern)
